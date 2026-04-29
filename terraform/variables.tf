85d0cdaf72cd3d130e2c9d1c737017e6ca0db999
jaxjoelliott <jaxjoelliott33@gmail.com>
2026-04-29 11:21:13 -0400

    feat/Managed first resources with Terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,6 @@
-variable "table_name" {
-  description = "Name of DynamoDB table"
-  type        = string
-  default     = "job-applications"
-}
-
+#region name
 variable "region_name" {
-  description = "AWS region name"
+  description = "Name of the AWS region"
   type        = string
   default     = "us-east-1"
 }
